c28f955d427d18fcc374a01de62a823402c1486d
Bryan Shin <bryanshin1997@gmail.com>
2020-01-12 22:09:19 -0500

    finished part one of tutorial

---
+++ Assets/Scripts/Draw.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Draw : MonoBehaviour
+{
+    public bool mouseLookTesting;
+ 
+    private float pitch = 0;
+    private float yaw = 0;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseLookTesting)
+        {
+            yaw += 2 * Input.GetAxis("Mouse X");
+            pitch -= 2 * Input.GetAxis("Mouse Y");
+ 
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+ 
+}
